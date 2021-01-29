74280b2e7fb55caab93496e69f64f9c926d3a2a3
cw <illumea@gmail.com>
2021-01-29 12:09:42 +0800

    add getEvent, delete & update routes

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -28,11 +28,36 @@ const eventControllers = {
           }
 
     },
+
+    getEvent: async (req, res) => {
+        const { id } = req.params;
+
+        try {
+          const event = await eventModel.findOne({ _id: id })
+    
+          if (!event) {
+            res.status(404).json({
+              sucess: false,
+              message: "Event not found",
+            });
+            return;
+          }
+    
+          res.status(200).json({
+            success: true,
+            event,
+          });
+        } catch (err) {
+          res.status(500).json({
+            success: false,
+            message: err.message,
+          });
+        }
+
+    },
   
     createEvent: async (req, res) => {
-
         const { id, calendarId, title, category, dueDateClass } = req.body;
-
         console.log(req.body);
 
         try {
@@ -51,11 +76,6 @@ const eventControllers = {
             title,
             category,
             dueDateClass,
-            // id: req.body.id,
-            // calendarId: req.body.calendarId,
-            // title: req.body.title,
-            // category: req.body.category,
-            // dueDateClass: req.body.dueDateClass,
           });
     
 
@@ -71,6 +91,92 @@ const eventControllers = {
           });
         }
     },
+
+    updateEvent: async (req, res) => {
+        const { id } = req.params;
+        // const { calendarId } = req.body;
+        // const { calendarId, title, category, dueDateClass } = req.body;
+
+        try {
+
+        // check if any input fields were left blank
+        const eventUpdates = {};
+        const formValues = req.body;
+        let updateNum = 0;
+
+        for (const prop in formValues) {
+            if (formValues[prop]) {
+            eventUpdates[prop] = formValues[prop];
+            updateNum += 1;
+            }
+        }
+
+        if (updateNum === 0) {
+            res.statusCode = 406; // 406-not acceptable
+            res.json({
+            success: false,
+            message: "No fields were updated. Please try again.",
+            });
+            return;
+        }
+
+        // check if event exists
+        let event = await eventModel.findOne({ _id: id });
+    
+          if (!event) {
+            res.status(404).json({
+              success: false,
+              message: "Event not found",
+            });
+            return;
+          }
+
+        event = await eventModel.findOneAndUpdate(
+            {
+                _id: id,
+            }, 
+            {
+                $set: eventUpdates,
+            });
+    
+          res.status(200).json({
+            success: true,
+            message: "Event updated",
+          });
+        } catch (err) {
+          res.status(500).json({
+            success: false,
+            message: err.message,
+          });
+        }
+
+    },
+
+    deleteEvent: async (req, res) => {
+        const { id } = req.params;
+
+        try {
+          const response = await eventModel.deleteOne({ _id: id });
+    
+          if (response.n === 0) {
+            res.status(404).json({
+              success: false,
+              message: "Event not found",
+            });
+            return;
+          }
+    
+          res.status(200).json({
+            success: true,
+            message: "Event deleted",
+          });
+        } catch (err) {
+          res.status(500).json({
+            success: false,
+            message: "Error deleting event",
+          });
+        }
+      },
 
 };
 
